7b5ac92c5d2fd3ebd7878c2c5d0b80e1fffe92a7
rusikstayhome <alenrus32@gmail.com>
2022-07-14 11:52:25 +0300

    add a loop

--- js/script.js
+++ js/script.js
@@ -9,12 +9,31 @@ const personalMovieDB = {
 };
 
 
-const a = prompt('Один из последних просмотренных фильмов?', ""),
-    b = prompt('На сколько оцените?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ""),
-    d = prompt('На сколько оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ""),
+        b = prompt('На сколько оцените?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман!');
+} else {
+    alert('Ошибка!');
+}
+
 
 console.log(personalMovieDB);
+
+///////////////
+
